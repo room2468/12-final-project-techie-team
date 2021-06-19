22f56ad556503447490d0b2f7aaffb47ac526418
Evita Putri Nauli <eputrinauli@gmail.com>
2021-06-19 20:48:21 +0700

    Mengedit tampilan

--- lib/pages/recommendation.dart
+++ lib/pages/recommendation.dart
@@ -1,41 +1,48 @@
+import 'package:final_project/pages/home.dart';
 import 'package:final_project/pages/woman.dart';
 import 'package:final_project/pages/man.dart';
 import 'package:flutter/material.dart';
 
-class Recommendation1 extends StatefulWidget {
+class Recommendation extends StatefulWidget {
   @override
-  Recommendation1State createState() => Recommendation1State();
+  RecommendationState createState() => RecommendationState();
 }
 
-class Recommendation1State extends State<Recommendation1> {
+class RecommendationState extends State<Recommendation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Recommendation Parfume'),
           backgroundColor: Colors.teal,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Home()),
+              );
+            },
+          ),
         ),
         backgroundColor: Colors.black,
         body: Column(children: [
           Container(
-            child: Column(
-              children: <Widget>[
-                // Foto utama
-                new Image.asset('img/woman.jpeg'),
-              ],
-            ),
-          ),
-          //button recomendation
-          Container(
-            height: 50,
-            width: 250,
+            height: 350,
+            width: 470,
             margin: EdgeInsets.all(10),
             child: RaisedButton(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(18.0),
                   side: BorderSide(color: Color.fromRGBO(0, 0, 0, 1))),
               child: Column(
-                children: [
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  // Foto utama
+                  new Image.asset('img/woman.jpeg'),
                   Text("WOMAN",
                       style: TextStyle(
                           fontSize: 27, color: Color.fromRGBO(0, 0, 0, 1))),
@@ -51,24 +58,18 @@ class Recommendation1State extends State<Recommendation1> {
           ),
           //button custome
           Container(
-            child: Column(
-              children: <Widget>[
-                // Foto utama
-                new Image.asset('img/man.jpeg'),
-              ],
-            ),
-          ),
-          //button recomendation
-          Container(
-            height: 50,
-            width: 250,
+            height: 380,
+            width: 470,
             margin: EdgeInsets.all(10),
             child: RaisedButton(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(18.0),
                   side: BorderSide(color: Color.fromRGBO(0, 0, 0, 1))),
               child: Column(
-                children: [
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  // Foto utama
+                  new Image.asset('img/man.jpeg'),
                   Text("MAN",
                       style: TextStyle(
                           fontSize: 27, color: Color.fromRGBO(0, 0, 0, 1))),
